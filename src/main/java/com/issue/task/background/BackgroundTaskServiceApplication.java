f3b416a37578c94f2cd7873abe67093fcb91903f
DanilaShapkin <danila.shapkin@gmail.com>
2025-03-10 21:49:36 +0100

    base init

---
+++ src/main/java/com/issue/task/background/BackgroundTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.issue.task.background;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackgroundTaskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issue/task/background/BackgroundTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issue.task.background;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
